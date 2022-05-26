71fac4aac5eded9904cfe9368c4cd0825246e49b
Lukas Jorgensen <lukas.jorgensen@blockfi.com>
2022-05-25 21:08:19 -0400

    glitchy's rusty solution -- make sure to read the README

---
+++ glitchys_solution/README.md
@@ -0,0 +1,16 @@
+# Overview
+Welcome to Rust! Rust can create a binary executables that, once generated, can be run on ANY operating systems, therefore you do not need to install Rust to run this program! 
+
+## INSTRUCTIONS
+1. Open your terminal and navigate to the `./glitchys_solution/`
+2. Complete the following commands below specific to your OS:
+
+### MacOS
+```
+./target/debug/glitchys_solution`
+```
+
+### Windows
+```
+./target/x86_64-pc-windows-gnu/debug/glitchys_solution.exe`
+```
---
+++ glitchys_solution/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // create empty vector (growable collection) and fill it up with 0-100 (who wants to type all that?!)
+    let mut v1 = Vec::new();
+    let mut v2 = Vec::new();
+
+    // loop through range and build both vectors
+    for v in 0..=100 {
+        if v % 2 == 0 && v % 5 == 0 {
+            v1.push(v)
+        } else {
+            v2.push(v)
+        }
+    };
+
+    println!("\nNumbers divisible by 2 AND 5:");
+    v1.iter().for_each(|v| println!("{}", v));
+
+    println!("\nNumbers that are NOT divisible by 2 AND 5:");
+    v2.iter().for_each(|v| println!("{}", v));
+}
