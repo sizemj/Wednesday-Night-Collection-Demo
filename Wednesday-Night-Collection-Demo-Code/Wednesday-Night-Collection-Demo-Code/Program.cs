6ca94421667ea415ed3ef2ec17b552413e200a3b
sizemj <sizemj@gmail.com>
2022-05-25 20:08:22 -0400

    First Commit and Setup

---
+++ Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wednesday_Night_Collection_Demo_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
