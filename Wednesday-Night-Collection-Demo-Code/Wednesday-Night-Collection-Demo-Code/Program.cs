57a900ba5b7b6faa69efd8dabdad4df4bbba197f
Andrew J Hughes <andrew@ajhughes.dev>
2022-05-29 04:27:13 -0400

    Update Program.cs

--- Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
+++ Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
@@ -6,7 +6,7 @@ namespace Wednesday_Night_Collection_Demo_Code
   {
     static void Main(string[] args)
     {
-      for (int i = 1; i <= 100; i++)
+      for (int i = 0; i <= 100; i++)
       {
         if (i % 2 == 0 && i % 5 == 0)
         {
