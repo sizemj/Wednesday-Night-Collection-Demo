e288b80530b7ab79e53126da297bdad86862bf3d
Andrew J Hughes <andrew@ajhughes.dev>
2022-05-29 03:58:06 -0400

    program.cs

--- Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
+++ Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
-
-namespace Wednesday_Night_Collection_Demo_Code
+﻿namespace Wednesday_Night_Collection_Demo_Code
 {
-    internal class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      for (int i = 1; i <= 100; i++)
+      {
+        if (i % 2 == 0 && i % 5 == 0)
         {
-            Console.WriteLine("Hello World!");
+          Console.WriteLine(i + " is divisible by two and five.");
         }
+        else if (i % 2 == 0)
+        {
+          Console.WriteLine(i + " is divisible by two.");
+        }
+        else if (i % 5 == 0)
+        {
+          Console.WriteLine(i + " is divisible by five.");
+        }
+        else
+        {
+          Console.WriteLine(i + " is not divisible by two or five.");
+        }
+      }
     }
-}
+  }
