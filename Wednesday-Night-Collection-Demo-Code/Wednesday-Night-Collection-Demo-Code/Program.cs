601714d2c853ce8b001551b1774c7b4e3250d569
James Petross <jamespetross@gmail.com>
2022-05-30 00:12:44 -0400

    Eureka! Finally figured it out

--- Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
+++ Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Wednesday_Night_Collection_Demo_Code
 {
-    internal class Program
+    public class example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " is divisible by both!");
+                }
+                else if (i % 2 == 0)
+                {
+                    Console.WriteLine(i + " is divisible by two");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " is divisible by five");
+                }
+                else
+                {
+                    Console.WriteLine(i + " indivisible by either number");
+                }
+            }
         }
     }
 }
