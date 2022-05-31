7017bc6aa16fa9e534ce274023fa571dab8c41af
jsizemo <jason.sizemore@e-farmcredit.com>
2022-05-31 14:00:25 -0400

    My Solution Commit

--- Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
+++ Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Wednesday_Night_Collection_Demo_Code
 {
-    internal class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i < 101; i++)
+
+                if (i % 2 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " is divisible by 2 and 5");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + " is divisible by 5");
+                }
+                else if (i % 2 == 0)
+                {
+                    Console.WriteLine(i + " is divisible by 2");
+                }
+                else if (i % 2 != 0 || i % 5 != 0)
+                {
+                    Console.WriteLine(i + " Is not divisible by 2 or 5");
+                }
+            Console.ReadLine();
         }
     }
 }
