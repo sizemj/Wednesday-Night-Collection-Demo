9f0a5d77486c95f50dea2a97085b05a4c0b1f2f5
Chris Hardin <hardin.christopher93@gmail.com>
2022-06-01 17:09:10 -0400

    Cleaner, updated version
    This cleans up the code by separating things into methods.

--- Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
+++ Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
@@ -7,87 +7,87 @@ namespace Wednesday_Night_Collection_Demo_Code
     {
         static void Main(string[] args)
         {
-            bool div2;
-            bool div5;
-            bool div2and5;
-            //float result;
+            bool methodSucceeded = DetermineDivisible();
+            if (methodSucceeded)
+            {
+                Console.WriteLine("Done determining divisibility!" + "\n");
+                PlayASong();
+            }
+        }
+
+        //Put the for loop into a method to make the above easier to deal with.
+        static bool DetermineDivisible()
+        {
             for (int i = 0; i <= 100; i++)
             {
-                //For some reason, I could not get this to work when declaring the booleans above as false and not including else statements below.
-                //The variables div2 and div5 would always return true no matter what was in the actual if statement. This may have been covered in class, but I'm not sure the reasoning behind that.
-                //It was driving me crazy, and I thought the code commented out below was what fixed it, but adding the else statements did the trick with a simpler calculation.
 
-                //result = ((float)i / 2);
-                //if (Math.Floor(result) == result)
-                //{
-                //    div2 = true;
-                //}
-                //else
-                //{
-                //    div2 = false;
-                //}
-
-                //Determine whether i is divisible by 2, 5, or both 2 and 5.
-                if (i % 2 == 0)
-                {
-                    div2 = true;
-                }
-                else
-                {
-                    div2 = false;
-                }
-
-                if (i % 5 == 0)
-                {
-                    div5 = true;
-                }
-                else
-                {
-                    div5 = false;
-                }
-
-                if (div2 == true && div5 == true)
-                {
-                    div2and5 = true;
-                }
-                else
-                {
-                    div2and5 = false;
-                }
-
-                //Print a message to the console for each number based on the results.
-                //This could definitely be done much cleaner. Maybe another night.
-                if (div2 == false && div5 == false && div2and5 == false)
+                //Determine whether i is divisible by 2, 5, or both 2 and 5, then print a message.
+                if (Divisible(i, 2) == false && Divisible(i, 5) == false)
                 {
                     Console.WriteLine("The number " + i + " is not divisible by 2 or 5.");
                 }
-                else if (div2and5 == true)
+                else if (Divisible(i, 2) == true && Divisible(i, 5) == true)
                 {
                     Console.WriteLine("The number " + i + " is divisible by both 2 and 5.");
                 }
-                else if (div2 == true && div5 == false)
+                else if (Divisible(i, 2) == true && Divisible(i, 5) == false)
                 {
                     Console.WriteLine("The number " + i + " is divisible by 2 but not 5.");
                 }
-                else if (div2 == false && div5 == true)
+                else if (Divisible(i, 2) == false && Divisible(i, 5) == true)
                 {
                     Console.WriteLine("The number " + i + " is divisible by 5 but not 2.");
                 }
                 else
                 {
                     Console.WriteLine("Something crazy happened with " + i + "!");
+                    return false;
                 }
             }
-#pragma warning disable CA1416 // Validate platform compatibility
-            System.Media.SoundPlayer ik = new SoundPlayer();
-#pragma warning restore CA1416 // Validate platform compatibility
-#pragma warning disable CA1416 // Validate platform compatibility
-            ik.SoundLocation = Environment.CurrentDirectory + "/ik.wav";
-#pragma warning restore CA1416 // Validate platform compatibility
-#pragma warning disable CA1416 // Validate platform compatibility
-            ik.Play();
-#pragma warning restore CA1416 // Validate platform compatibility
-            Console.ReadKey();
+            return true;
         }
+
+        
+       //Method to determine if numbers are divisible
+        static bool Divisible(int num1, int num2)
+        {
+            int numVar = (num1 % num2);
+            if (numVar == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Just for fun
+        static void PlayASong()
+        {
+            Console.WriteLine("Would you like to hear a song?");
+            string answer = Console.ReadLine();
+            if (answer == "yes" || answer == "Yes" || answer == "y" || answer == "Y")
+            {
+                try
+                {
+                    System.Media.SoundPlayer ik = new SoundPlayer();
+                    ik.SoundLocation = Environment.CurrentDirectory + "/ik.wav";
+                    ik.Play();
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception occurred: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry, maybe next time!");
+                Console.ReadLine();
+            }
+
+        }
+
     }
 }
