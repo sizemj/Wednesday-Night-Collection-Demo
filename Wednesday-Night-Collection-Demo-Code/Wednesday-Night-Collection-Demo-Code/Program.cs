a1f70d39546d3ea65e3dd0ae29386a98610c983b
Jason Sizemore <sizemj@gmail.com>
2022-06-01 14:29:05 -0400

    Update Program.cs
    Rolled back to a blank project

--- Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
+++ Wednesday-Night-Collection-Demo-Code/Wednesday-Night-Collection-Demo-Code/Program.cs
@@ -6,44 +6,7 @@ namespace Wednesday_Night_Collection_Demo_Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("List of numbers divisible by 2");
-int i = 0;
-while (i < 101) 
-{
-   if ((i % 2) == 0)
-            {
-                Console.WriteLine(i);
-            }
-    i = i + 1;
-}
 
-
- Console.WriteLine("List of numbers divisible by 5");
-int a = 0;
-while (a < 101) 
-{
-   if ((a % 5) == 0)
-            {
-                Console.WriteLine(a);
-            }
-    a = a + 1;
-}
-
-
- Console.WriteLine("List of numbers divisible by 2 and 5");
-int b = 0;
-while (b< 101) 
-{
-   if(((b % 2) == 0) and ((b % 5) == 0)))
-            {
-                Console.WriteLine(b);
-            }
-            elseif((b % 2) != 0) or ((b % 5) != 0))
-            {
-              Console.WriteLine("the numbers is not divisible by 2 or 5") 
-            }
-    b = b + 1;
-}
         }
-    }
+
 }
